1fcf6d10531d4cdd2aaeb32db7ab9c0e23fc8553
Akash Paliwal <akash@newput.com>
2023-04-02 17:29:23 +0530

    Added code in main

--- src/file1.js
+++ src/file1.js
@@ -5,5 +5,9 @@ f1.push('Paliwal')
 f1.push('Software')
 f1.push('Engineer')
 
+const name1 = 'Akash Paliwal'
+
 f1.forEach(f => f+'123');
 console.log(f1)
+
+console.log(name1);
--- src/file2.js
+++ src/file2.js
@@ -1,7 +1,12 @@
 const f2 = ['Hi', 'How', 'are', 'you'];
-
+const f5 = [1, 2, 3, 4, 5, 6, 7];
 
 f2.map(f => console.log(f))
 
 f2.includes('hello')
 console.log('123')
+
+
+const found = f5.find(f => f == 5)
+
+console.log(found);
