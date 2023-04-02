5544703aadde9e6231d893b4ed8c2cd535732260
Akash Paliwal <akash@newput.com>
2023-04-02 17:24:15 +0530

    Added test code

--- src/file1.js
+++ src/file1.js
@@ -1,1 +1,9 @@
 const f1 = [];
+
+f1.push('Akash')
+f1.push('Paliwal')
+f1.push('Software')
+f1.push('Engineer')
+
+f1.forEach(f => f+'123');
+console.log(f1)
--- src/file2.js
+++ src/file2.js
@@ -1,1 +1,7 @@
-const f2 = [];
+const f2 = ['Hi', 'How', 'are', 'you'];
+
+
+f2.map(f => console.log(f))
+
+f2.includes('hello')
+console.log('123')
