2815288dc71b5e968af47f4bdb824c2226a31b28
Akash Paliwal <akash@newput.com>
2023-04-02 17:38:44 +0530

    Added code in main c3

--- src/file2.js
+++ src/file2.js
@@ -2,6 +2,9 @@ const f2 = ['Hi', 'How', 'are', 'you'];
 const f5 = [1, 2, 3, 4, 5, 6, 7];
 
 f2.map(f => console.log(f))
+const f6 = [1, 2, 3, 5, 5, 6, 7];
+console.log(f6)
+
 
 f2.includes('hello')
 console.log('000')
